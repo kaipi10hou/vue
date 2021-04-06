b23d416c5035d82b78bc0d6536824d12015c7af7
CherryOnTop <kaipi10hou@gmail.com>
2021-04-06 23:07:20 +0900

    vue로 채팅만들기

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>대화명을 입력해 주세요!</div>
+    <!-- <input type="text" v-model="usernick"> -->
+    <input type="text" @keydown.enter="submit" @input="changedNick" class="" :class="usernick =='김의석'?'a':'b'">
+    <button @click="submit">입장</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
+  },
+  data(){
+    return{
+      usernick:"",
+    }
+  },
+  watch:{
+    'usernick':function(value){
+      console.log(value);
+    }
+  },
+  methods:{
+    changedNick(e){
+      this.usernick = e.target.value;
+    },
+    submit(){
+      console.log("submit");
+    }
   }
 }
 </script>
